7cc21c305d69a897520ce5ef8f17dc84729ceddf
yuong1979 <yuong1979@gmail.com>
2022-07-28 22:31:52 +0800

    added flask + firebase authentication

---
+++ hashing.py
@@ -0,0 +1,14 @@
+import bcrypt
+
+#this process encodes the password
+# password = 'SecretPassword'
+# password = password.encode("utf-8")
+
+#this is similar to the above - which encodes the password
+password = b'SecretPassword'
+hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+
+if bcrypt.checkpw(password, hashed):
+    print ("password match")
+else:
+    print ("password not match")
