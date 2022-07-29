294d17bbfebc595d883fc145b22fafd832a972db
yuong1979 <yuong1979@gmail.com>
2022-07-29 09:32:44 +0800

    connected streamlit authenticator

--- app.py
+++ app.py
@@ -1,67 +1,149 @@
-import pyrebase
-from firebase_admin import auth
-from flask import Flask, session, render_template, request, redirect
-import json
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+import pandas as pd
+
+import database as db
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 
-app = Flask(__name__)
+# --- USER AUTHENTICATION ---
+# names = ["Peter Parker", "Rebecca Miller"]
+# usernames = ["pparker", "rmiller"]
 
-with open('config.json') as f:
-    config = json.load(f)
+# # load hashed passwords
+# file_path = Path(__file__).parent / "hashed_pw.pkl"
+# with file_path.open("rb") as file:
+#     hashed_passwords = pickle.load(file)
 
-firebase = pyrebase.initialize_app(config) 
-auth = firebase. auth()
-
-app.secret_key = 'secret'
-
-@app.route('/', methods=['POST', 'GET']) 
-def index():
-    print (session)
-    if ('user' in session):
-        print ("user in session")
-        return 'Hi, {}'.format(session['user'])
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        try:
-            user = auth.sign_in_with_email_and_password(email, password)
-            print ("signed in")
-            session['user'] = email
-        except:
-            print ("failed")
-            return 'Failed to login'
-    return render_template('home.html')
-
-@app.route('/logout') 
-def logout():
-    session.pop('user')
-    print ('logged out')
-    return redirect('/')
-
-if __name__ == '__main__':
-    app.run(port=1111)
+users = db.fetch_all_users()
+usernames = [user["key"] for user in users]
+names = [user["name"] for user in users]
+hashed_passwords = [user["password"] for user in users]
 
 
 
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
 
-# email = "test2@gmail.com"
-# password = "123456"
+name, authentication_status, username = authenticator.login("Login", "main")
 
-# ## for creating new user data on new account
-# user = auth.create_user_with_email_and_password(email, password)
-# print (user)
+if authentication_status == False:
+    st.error("Username/password is incorrect")
 
-# ## for retriveving user data
-# info = auth.get_account_info(user['idToken'])
-# print (info)
+if authentication_status == None:
+    st.warning("Please enter your username and password")
 
-# ## email verification
-# auth.send_email_verification(user['idToken'])
+if authentication_status:
+    
+    # ---- READ CSV ----
+    @st.cache
+    def get_data_from_csv():
+        df = pd.read_csv('sales.csv')
+        # Add 'hour' column to dataframe
+        df["hour"] = pd.to_datetime(df["Time"], format="%H:%M").dt.hour
+        return df
 
-# ## for password reset
-# auth.send_password_reset_email(email)
+    df = get_data_from_csv()
 
-# ## list of methods inside auth
-# # 'api_key', 'create_custom_token', 'create_user_with_email_and_password', 'credentials', 'current_user', 'delete_user_account', 'get_account_info', 'refresh', 'requests', 'send_email_verification', 'send_password_reset_email', 'sign_in_anonymous', 'sign_in_with_custom_token', 'sign_in_with_email_and_password', 'verify_password_reset_code'
+    # ---- SIDEBAR ----
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+    st.sidebar.header("Please Filter Here:")
+    city = st.sidebar.multiselect(
+        "Select the City:",
+        options=df["City"].unique(),
+        default=df["City"].unique()
+    )
 
-# Based on a tutorial by https://youtu.be/HltzFtn9f1c
+    customer_type = st.sidebar.multiselect(
+        "Select the Customer Type:",
+        options=df["Customer_type"].unique(),
+        default=df["Customer_type"].unique(),
+    )
+
+    gender = st.sidebar.multiselect(
+        "Select the Gender:",
+        options=df["Gender"].unique(),
+        default=df["Gender"].unique()
+    )
+
+    df_selection = df.query(
+        "City == @city & Customer_type ==@customer_type & Gender == @gender"
+    )
+
+    # ---- MAINPAGE ----
+    st.title(":bar_chart: Sales Dashboard")
+    st.markdown("##")
+
+    # TOP KPI's
+    total_sales = int(df_selection["Total"].sum())
+    average_rating = round(df_selection["Rating"].mean(), 1)
+    star_rating = ":star:" * int(round(average_rating, 0))
+    average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+    left_column, middle_column, right_column = st.columns(3)
+    with left_column:
+        st.subheader("Total Sales:")
+        st.subheader(f"US $ {total_sales:,}")
+    with middle_column:
+        st.subheader("Average Rating:")
+        st.subheader(f"{average_rating} {star_rating}")
+    with right_column:
+        st.subheader("Average Sales Per Transaction:")
+        st.subheader(f"US $ {average_sale_by_transaction}")
+
+    st.markdown("""---""")
+
+    # SALES BY PRODUCT LINE [BAR CHART]
+    sales_by_product_line = (
+        df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+    )
+    fig_product_sales = px.bar(
+        sales_by_product_line,
+        x="Total",
+        y=sales_by_product_line.index,
+        orientation="h",
+        title="<b>Sales by Product Line</b>",
+        color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+        template="plotly_white",
+    )
+    fig_product_sales.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=False))
+    )
+
+    # SALES BY HOUR [BAR CHART]
+    sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
+    fig_hourly_sales = px.bar(
+        sales_by_hour,
+        x=sales_by_hour.index,
+        y="Total",
+        title="<b>Sales by hour</b>",
+        color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+        template="plotly_white",
+    )
+    fig_hourly_sales.update_layout(
+        xaxis=dict(tickmode="linear"),
+        plot_bgcolor="rgba(0,0,0,0)",
+        yaxis=(dict(showgrid=False)),
+    )
+
+
+    left_column, right_column = st.columns(2)
+    left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+    right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+
+    # ---- HIDE STREAMLIT STYLE ----
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
---
+++ flask_app.py
@@ -0,0 +1,67 @@
+import pyrebase
+from firebase_admin import auth
+from flask import Flask, session, render_template, request, redirect
+import json
+
+
+app = Flask(__name__)
+
+with open('firebase_app_config.json') as f:
+    config = json.load(f)
+
+firebase = pyrebase.initialize_app(config) 
+auth = firebase. auth()
+
+app.secret_key = 'secret'
+
+@app.route('/', methods=['POST', 'GET']) 
+def index():
+    print (session)
+    if ('user' in session):
+        print ("user in session")
+        return 'Hi, {}'.format(session['user'])
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            print ("signed in")
+            session['user'] = email
+        except:
+            print ("failed")
+            return 'Failed to login'
+    return render_template('home.html')
+
+@app.route('/logout') 
+def logout():
+    session.pop('user')
+    print ('logged out')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(port=1111)
+
+
+
+
+# email = "test2@gmail.com"
+# password = "123456"
+
+# ## for creating new user data on new account
+# user = auth.create_user_with_email_and_password(email, password)
+# print (user)
+
+# ## for retriveving user data
+# info = auth.get_account_info(user['idToken'])
+# print (info)
+
+# ## email verification
+# auth.send_email_verification(user['idToken'])
+
+# ## for password reset
+# auth.send_password_reset_email(email)
+
+# ## list of methods inside auth
+# # 'api_key', 'create_custom_token', 'create_user_with_email_and_password', 'credentials', 'current_user', 'delete_user_account', 'get_account_info', 'refresh', 'requests', 'send_email_verification', 'send_password_reset_email', 'sign_in_anonymous', 'sign_in_with_custom_token', 'sign_in_with_email_and_password', 'verify_password_reset_code'
+
+# Based on a tutorial by https://youtu.be/HltzFtn9f1c
