0c4ab84c4c353016b160490f8979d00764c7beb4
yuong1979 <yuong1979@gmail.com>
2022-10-07 10:45:04 +0800

    deploy to production on docker works / added data stats to dashboard

--- admin_dashboard.py
+++ admin_dashboard.py
@@ -17,6 +17,45 @@ def task():
     print ('start task')
     threading.Thread(target=update_data).start()
 
+def refresh():
+    st.experimental_rerun()
+
+def data_stats():
+        try:
+            eq_daily_agg = pd.read_pickle('data/eq_daily_agg.pickle')
+            st.write("eq_daily_agg " + "loaded")
+        except:
+            st.write("eq_daily_agg " + "NOT loaded")
+        try:
+            eq_daily_industry = pd.read_pickle('data/eq_daily_industry.pickle')
+            recordtime = eq_daily_industry['daily_agg_record_time'].max()
+            st.write("eq_daily_industry " + "latest loaded : " + str(recordtime))
+        except:
+            st.write("eq_daily_industry " + "NOT loaded")
+        try:
+            eq_daily_kpi = pd.read_pickle('data/eq_daily_kpi.pickle')
+            recordtime = eq_daily_kpi['updated_datetime'].max()
+            st.write("eq_daily_kpi " + "latest loaded : " + str(recordtime))
+        except:
+            st.write("eq_daily_kpi " + "NOT loaded")
+        try:
+            eq_hist_details = pd.read_pickle('data/eq_hist_details.pickle')
+            recordtime = eq_hist_details['date'].max()  
+            st.write("eq_hist_details " + "latest loaded : " + str(recordtime))
+        except:
+            st.write("eq_hist_details " + "NOT loaded")
+        try:
+            eq_hist_price = pd.read_pickle('data/eq_hist_price.pickle')
+            recordtime = eq_hist_price['date'].max()  
+            st.write("eq_hist_price " + "latest loaded : " + str(recordtime))
+        except:
+            st.write("eq_hist_price " + "NOT loaded")
+        try:
+            eq_hist_sum = pd.read_pickle('data/eq_hist_sum.pickle')
+            recordtime = eq_hist_sum['qtr_last_date'].max()  
+            st.write("eq_hist_sum " + "latest loaded : " + str(recordtime))
+        except:
+            st.write("eq_hist_sum " + "NOT loaded")
 
 
 def ad_dash():
@@ -36,7 +75,13 @@ def ad_dash():
             st.write("Logged in as admin: ", email)
             st.write("Last download datetime is: ", earliest_datetime)
 
+            data_stats()
+
             st.button("Run download Data", key=None, help=None, on_click=task, disabled=False)
+
+            st.button("Refresh page", key=None, help=None, on_click=refresh, disabled=False)
+
+
     except:
         pass        
 
